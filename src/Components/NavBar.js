fb2650223d4710df9635e67c9d6bdb8f85a39477
SafoPost <s.i.post@mail.ru>
2020-10-29 23:05:34 +0500

    feat add NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg';
+import signImg from '../image/sign.svg';
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  width: 100vw;
+  height: 80px;
+  background-color: #299b01;
+`;
+
+const Container = styled.div`
+  width: 1440px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  color: #ffffff;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const BtnSign = styled.button`
+  min-width: 55px
+  padding: 0 5px;
+  border: none;
+  background-color: transparent;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImgSign = styled.img`
+  width: 28px;
+  margin-bottom: 3px;
+`;
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Container>
+      <Logo>
+        <ImgLogo src={logoImg} alt="logo" />
+        <H1>MrDonald's</H1>
+      </Logo>
+      <BtnSign>
+        <ImgSign src={signImg} alt="sign" />
+        Войти
+      </BtnSign>
+    </Container>
+  </NavBarStyled>
+);
